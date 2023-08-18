888a5cc9f71c3cc9d331a80cd670091d91d3ecaa
username <youremail@domain.com>
2023-08-18 14:25:20 -0400

    Detail Page Boilerplate

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,20 +6,25 @@ from django.template import loader
 
 from django.shortcuts import render
 
+from django.http import Http404
+
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    context = {
-        "latest_question_list": latest_question_list,
-    }
+    context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exisit")
+    return render(request, "polls/detail.html", {"question": question})    
+
+    
 
 
 def results(request, question_id):
